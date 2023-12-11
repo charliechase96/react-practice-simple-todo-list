c3fc9a5094e9a9794821455dca4bc4af2612edcf
Charlie Gray <charliechasegray@gmail.com>
2023-12-11 09:16:48 -0800

    add imports/exports and TodoList component to App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./TodoList";
+import AddTodo from "./AddTodo";
+
+function App() {
+    return (
+        <div>
+            <TodoList/>
+        </div>
+    )
+}
+
+export default App;
