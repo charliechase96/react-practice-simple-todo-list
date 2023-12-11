2faf2fb6254f9633969dd68527373bb2f1ea3c0b
Charlie Gray <charliechasegray@gmail.com>
2023-12-11 09:25:33 -0800

    add Todo file and add function to TodoList component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,13 @@
 import React from "react";
 import AddTodo from "./AddTodo";
+import Todo from "./Todo";
 
+function TodoList() {
+    return (
+        <div>
+            <AddTodo/>
+            {/* add todos here in list form */}
+        </div>
+    )
+}
 export default TodoList;
